cdde099034de2032b231808d8c8c2aa8f17ac7a9
Ahmed <ahmedbeingprogramer>
2023-10-14 17:27:18 +0300

    first commit

---
+++ lib/bloc/weather_bloc_bloc.dart
@@ -0,0 +1,29 @@
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
+
+part 'weather_bloc_event.dart';
+part 'weather_bloc_state.dart';
+
+const String _apiKey = "Api_Key";
+
+class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
+  WeatherBlocBloc() : super(WeatherBlocInitial()) {
+    on<FetchWeather>((event, emit) async{
+      emit(WeatherBlocLoading());
+      try {
+        WeatherFactory wf = WeatherFactory(_apiKey,language: Language.ARABIC );
+
+        Weather weather = await wf.currentWeatherByLocation(
+          event.position.latitude,
+          event.position.longitude,
+        );
+        emit(WeatherBlocSuccess(weather));
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
+    });
+  }
+}
